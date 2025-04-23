33e865c8b1b969a9e2e85e5c189d703934348bf4
Maria Mendonca <maria.mendonca156@gmail.com>
2025-04-23 23:07:06 +0530

    Added basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react"; 
+import validator from 'validator'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => { 
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+	const [errorMessage, setErrorMessage] = useState('') 
+
+	const validate = (value) => { 
+
+		if (validator.isStrongPassword(value, { 
+			minLength: 8, minLowercase: 1, 
+			minUppercase: 1, minNumbers: 1, minSymbols: 1 
+		})) { 
+			setErrorMessage('Is Strong Password') 
+		} else { 
+			setErrorMessage('Is Not Strong Password') 
+		} 
+	} 
+
+	return ( 
+		<div style={{ 
+			marginLeft: '200px', 
+		}}> 
+			<pre> 
+				<h2>Checking Password Strength in ReactJS</h2> 
+				<span>Enter Password: </span><input type="text"
+					onChange={(e) => validate(e.target.value)}></input> <br /> 
+				{errorMessage === '' ? null : 
+					<span style={{ 
+						fontWeight: 'bold', 
+						color: 'red', 
+					}}>{errorMessage}</span>} 
+			</pre> 
+		</div> 
+	); 
+} 
 
 export default App
